0056aacd64ab4ca67fd02d187f628fd0ca84040c
Graham Boyle <olayile1999@gmail.com>
2024-01-16 19:30:41 +0100

    implememted dark mode

--- js/script.js
+++ js/script.js
@@ -24,3 +24,39 @@ document.addEventListener('DOMContentLoaded', () => {
     hambIcon.classList.add('fa-bars');
   });
 });
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+// scrollReveal
+
+const sections = document.querySelectorAll('[data-section]');
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      sections[i].classList.add('active');
+    } else {
+      sections[i].classList.remove('active');
+    }
+  }
+};
+
+scrollReveal();
+
+addEventOnElem(window, 'scroll', scrollReveal);
+
+// darkmode
+
+const switchTheme = document.querySelector("#switch");
+
+switchTheme.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
