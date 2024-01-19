8790f2441caaa5e0d61b4b0e9a569a1bd00b8cb1
Graham Boyle <olayile1999@gmail.com>
2024-01-19 11:09:50 +0100

    used strict in js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerButton = document.querySelector('.hamburger');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const hambIcon = document.querySelector('.hamburger i');
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const hambIcon = document.querySelector(".hamburger i");
 
   const toggleMenu = () => {
-    mobileMenu.classList.toggle('show');
-    hambIcon.classList.toggle('fa-bars');
-    hambIcon.classList.toggle('fa-xmark');
+    mobileMenu.classList.toggle("show");
+    hambIcon.classList.toggle("fa-bars");
+    hambIcon.classList.toggle("fa-xmark");
   };
 
-  hamburgerButton.addEventListener('click', toggleMenu);
+  hamburgerButton.addEventListener("click", toggleMenu);
 
-  mobileMenu.addEventListener('click', (e) => {
-    if (e.target.closest('.mobile-menu')) {
+  mobileMenu.addEventListener("click", (e) => {
+    if (e.target.closest(".mobile-menu")) {
       toggleMenu();
     }
   });
 
   // Reset menu state when the DOM is loaded
-  window.addEventListener('load', () => {
-    mobileMenu.classList.remove('show');
-    hambIcon.classList.remove('fa-xmark');
-    hambIcon.classList.add('fa-bars');
+  window.addEventListener("load", () => {
+    mobileMenu.classList.remove("show");
+    hambIcon.classList.remove("fa-xmark");
+    hambIcon.classList.add("fa-bars");
   });
 });
 
@@ -37,21 +39,21 @@ const addEventOnElem = function (elem, type, callback) {
 
 // scrollReveal
 
-const sections = document.querySelectorAll('[data-section]');
+const sections = document.querySelectorAll("[data-section]");
 
 const scrollReveal = function () {
   for (let i = 0; i < sections.length; i++) {
     if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
-      sections[i].classList.add('active');
+      sections[i].classList.add("active");
     } else {
-      sections[i].classList.remove('active');
+      sections[i].classList.remove("active");
     }
   }
 };
 
 scrollReveal();
 
-addEventOnElem(window, 'scroll', scrollReveal);
+addEventOnElem(window, "scroll", scrollReveal);
 
 // darkmode
 
